426214bdb81d58fa1a211e39998f854c4a495bfe
flannerymcnair <flannerymcnair@gmail.com>
2026-07-06 12:31:23 -0400

    finishing up

--- theme/forms/contact.php
+++ theme/forms/contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'flannerymcnair@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '/Users/flannerymcnair/Documents/GitHub/resume-website/theme/assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
